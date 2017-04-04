c2696b9d9d8172bd5286e284b50ffce2cbd18c02
eniallator <eniallatormc@gmail.com>
2017-04-04 19:33:01 +0100

    Promisified initDb

--- db/initDb.js
+++ db/initDb.js
@@ -4,27 +4,13 @@ var runDbQuery = require('./runDbQuery')
 const initTablesSql = fs.readFileSync('./db/sql/initTables.sql').toString()
 const insertGameMapSql = fs.readFileSync('./db/sql/insertGameMap.sql').toString()
 
-// Convert runDbQuery to use promises for fun and profit and money is good
+// Before this delete all -1(AKA default, system, blah blah blah) created maps
 
-runDbQuery(initTablesSql, null, (err) => {
-  if (err) {
-    console.log('Error initialising tables', err)
-  }
-
-  // Before this delete all -1(AKA default, system, blah blah blah) created maps
-  runDbQuery(insertGameMapSql, ['Level 1', fs.readFileSync('./uploads/Level 1.map'), -1], (err) => {
-    if (err) {
-      console.log('Error initialising data', err)
-    }
-
-    runDbQuery(insertGameMapSql, ['Level 2', fs.readFileSync('./uploads/Level 2.map'), -1], (err) => {
-      if (err) {
-        console.log('Error initialising data', err)
-      }
-
-      console.info('Database initialised')
-
-      process.exit()
-    })
-  })
-})
+runDbQuery(initTablesSql)
+  .then(() => runDbQuery(insertGameMapSql, ['Level 1', fs.readFileSync('./uploads/Level 1.map'), -1]))
+  .then(() => runDbQuery(insertGameMapSql, ['Level 2', fs.readFileSync('./uploads/Level 2.map'), -1]))
+  .then(() => {
+    console.info('Database initialised')
+  }).catch(err => {
+    console.error('Error initialising database', err)
+  }).then(process.exit)
--- db/runDbQuery.js
+++ db/runDbQuery.js
@@ -10,30 +10,34 @@ var config = {
   idleTimeoutMillis: 30000
 }
 
-function runDbQuery (querySql, values = [], callback) {
+function runDbQuery (querySql, values = []) {
   var pool = new pg.Pool(config)
-
-  pool.connect(function (err, client, done) {
-    if (err) {
-      return console.error('error fetching client from pool', err)
-    }
-
-    client.query(querySql, values, function (err, result) {
-      // call `done()` to release the client back to the pool
-      done()
-
+  return new Promise((resolve, reject) => {
+    console.log('in runDbQuery', querySql, values)
+    pool.connect((err, client, done) => {
       if (err) {
-        console.error('error running query', err)
-        callback(err)
+        return reject(err)
       }
 
-      callback(null, result.rows)
-    })
-  })
+      // console.log(querySql, values)
 
-  pool.on('error', function (err, client) {
-    console.error('idle client error', err.message, err.stack)
-    callback(err)
+      client.query(querySql, values, (err, result) => {
+        // call `done()` to release the client back to the pool
+        done()
+
+        if (err) {
+          console.error('error running query', err)
+          return reject(err)
+        }
+
+        resolve(result.rows)
+      })
+    })
+
+    pool.on('error', (err, client) => {
+      console.error('idle client error', err.message, err.stack)
+      reject(err)
+    })
   })
 }
 
